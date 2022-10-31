9a1d4bc966e12f4c15788b85fb1aabbab045ae00
kemalhaikal25 <115691135+kemalhaikal25@users.noreply.github.com>
2022-10-31 08:51:54 +0700

    Add files via upload

---
+++ OOP OLLA HAIKAL 12022213192/Menu.java
@@ -0,0 +1,18 @@
+class Menu{
+    
+    String menu;
+    String show_menu;
+    String search_menu;
+
+    public String getMenu(){
+        return menu;
+    }
+
+    public String getShowMenu(){
+        return show_menu;
+    }
+
+    public String getSearchMenu(){
+        return menu;
+    }
+}
